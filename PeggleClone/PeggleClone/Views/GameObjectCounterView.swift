6512db955145689d5901db4fc9e86c0f2b8a923e
sivayogasubramanian <sivayogasubramanian@gmail.com>
2022-02-27 14:21:35 +0800

    Minor change

--- PeggleClone/PeggleClone/Views/GameObjectCounterView.swift
+++ PeggleClone/PeggleClone/Views/GameObjectCounterView.swift
@@ -19,11 +19,22 @@ struct GameObjectCounterView: View {
     var body: some View {
         HStack {
             ImageCounterView(image: Constants.orangePegImage, count: orange, color: .black, isCircle: true)
-            ImageCounterView(image: Constants.bluePegImage, count: blue, color: .black, isCircle: true)
-            ImageCounterView(image: Constants.purplePegImage, count: purple, color: .black, isCircle: true)
-            ImageCounterView(image: Constants.grayPegImage, count: gray, color: .black, isCircle: true)
-            ImageCounterView(image: Constants.yellowPegImage, count: yellow, color: .black, isCircle: true)
-            ImageCounterView(image: Constants.pinkPegImage, count: pink, color: .black, isCircle: true)
+            if blue > 0 {
+                ImageCounterView(image: Constants.bluePegImage, count: blue, color: .black, isCircle: true)
+            }
+            if purple > 0 {
+                ImageCounterView(image: Constants.purplePegImage, count: purple, color: .black, isCircle: true)
+            }
+            if gray > 0 {
+                ImageCounterView(image: Constants.grayPegImage, count: gray, color: .black, isCircle: true)
+            }
+            if yellow > 0 {
+                ImageCounterView(image: Constants.yellowPegImage, count: yellow, color: .black, isCircle: true)
+
+            }
+            if pink > 0 {
+                ImageCounterView(image: Constants.pinkPegImage, count: pink, color: .black, isCircle: true)
+            }
             ImageCounterView(image: Constants.mainBallImage, count: balls, color: .black, isCircle: true)
         }
     }
